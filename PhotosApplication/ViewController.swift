8e40c9b9239bb7716d6dca2c25515b161788d1c2
Faizy51 <faizulla49@yahoo.co.uk>
2021-03-28 17:40:49 +0530

    Initial Commit

---
+++ PhotosApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosApplication
+//
+//  Created by Faizyy on 28/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
